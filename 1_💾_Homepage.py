a6229069df71c05c5cdd1739b514b62231763a40
ArchieSrii <126334262+ArchieSrii@users.noreply.github.com>
2023-02-26 12:01:13 +0530

    Update 1_💾_Homepage.py

--- 1_💾_Homepage.py
+++ 1_💾_Homepage.py
@@ -135,7 +135,7 @@ if ticker and stock_name2 is not None:
 
     def plot_decompose():
         decomp = seasonal_decompose(stock_close["Close"],period = int(len(stock_close)/6))  
-        st.pyplot(decomp.plot(),use_container_width=True) 
+        st.pyplot(decomp.plot(),use_container_width=True,x=None, y=None, width=0, height=0) 
         #st.line_chart(data=decomp.plot(), x=None, y=None, width=0, height=0, use_container_width=True)
         #st.pyplot()
     plot_decompose()
