ac4bf26d367640f45c52570f136b2417e08fe8f0
ArchieSrii <126334262+ArchieSrii@users.noreply.github.com>
2023-02-26 12:00:23 +0530

    Update 1_💾_Homepage.py

--- 1_💾_Homepage.py
+++ 1_💾_Homepage.py
@@ -135,9 +135,9 @@ if ticker and stock_name2 is not None:
 
     def plot_decompose():
         decomp = seasonal_decompose(stock_close["Close"],period = int(len(stock_close)/6))  
-        #st.pyplot(decomp.plot()) 
-        st.line_chart(data=decomp.plot(), x=None, y=None, width=0, height=0, use_container_width=True)
-        st.pyplot()
+        st.pyplot(decomp.plot(),use_container_width=True) 
+        #st.line_chart(data=decomp.plot(), x=None, y=None, width=0, height=0, use_container_width=True)
+        #st.pyplot()
     plot_decompose()
 
     #extracting trend and cyclicity using hpfilter
